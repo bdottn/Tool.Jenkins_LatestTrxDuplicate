f96fdb582fe69913cc6139ec481c2a98d9e18379
Echo Lee <bdottn.lee+git@gmail.com>
2018-03-01 14:01:17 +0800

    調整 Program 類別
    1.　加入方法：GenerateLatestTrxDuplicateArgument
    　1.1　依據傳入參數產生 LatestTrxDuplicateArgument 類別並填入相關屬性

--- LatestTrxDuplicate/Program.cs
+++ LatestTrxDuplicate/Program.cs
@@ -1,9 +1,54 @@
-﻿namespace LatestTrxDuplicate
+﻿using System;
+using System.Linq;
+
+namespace LatestTrxDuplicate
 {
     class Program
     {
         static void Main(string[] args)
         {
+            if (args == null && args.Length == 0)
+            {
+                throw new ArgumentNullException("參數錯誤：未傳入參數！");
+            }
+            else
+            {
+                var argument = GenerateLatestTrxDuplicateArgument(args);
+            }
+        }
+
+        private static LatestTrxDuplicateArgument GenerateLatestTrxDuplicateArgument(string[] args)
+        {
+            var argument = new LatestTrxDuplicateArgument();
+
+            foreach (var arg in args.Select(a => a.Split('=')))
+            {
+                switch (arg[0].ToLower())
+                {
+                    // Workspace Path
+                    case "-workspace":
+
+                        argument.Workspace = arg[1];
+
+                        break;
+
+                    // TestResultsFolder
+                    case "-testresultsfolder":
+
+                        argument.TestResultsFolder = arg[1];
+
+                        break;
+
+                    // Output FileName
+                    case "-outputfilename":
+
+                        argument.OutputFileName = arg[1];
+
+                        break;
+                }
+            }
+
+            return argument;
         }
     }
 }
